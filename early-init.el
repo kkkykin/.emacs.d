76e5a18c4dff7b72f78223f20435fefc186f9a16
kkky@bow <kkkykin@foxmail.com>
2023-10-07 22:11:33 +0800

    android: fix lib path

--- early-init.el
+++ early-init.el
@@ -31,9 +31,9 @@
           (format "%s:%s" "/data/data/com.termux/files/usr/bin"
                   (getenv "PATH")))
   (setenv "LD_LIBRARY_PATH"
-          (format "%s:%s"
-                  "/data/data/com.termux/files/usr/lib"
+          (format "%s:%s:%s"
 		  "/system/lib64"
+                  "/data/data/com.termux/files/usr/lib"
                   (getenv "LD_LIBRARY_PATH")))
   (push "/data/data/com.termux/files/usr/bin" exec-path)
   )
