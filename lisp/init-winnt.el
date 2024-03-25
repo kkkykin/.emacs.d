b7ec416b0e51d1dbe1de4e0f5c514d08aa3e45f8
kkky <kkkykin@foxmail.com>
2024-03-25 23:33:51 +0800

    windows coding-system fix

--- lisp/init-winnt.el
+++ lisp/init-winnt.el
@@ -24,59 +24,70 @@
 
 ;;; Code:
 
+(defun my/find-shell-command-coding-system (command)
+  "Find coding system for shell command."
+  (let ((program (seq-some (lambda (x) (and (not (string-prefix-p "/" x))
+                                        (not (string= "start" x)) x))
+                           (split-string-shell-command command))))
+    (find-operation-coding-system 'call-process program)))
+
 (defun my/advice-shell-command-coding-fix (orig-fun &rest args)
-  "Fix coding system for cmdproxy shell."
-  (if-let* ((command (car args))
-            (program-name
-             (seq-some
-              (lambda (x) (and (string-match-p "^[^\\(start\\|/.+\\)]" x) x))
-              (split-string-shell-command command)))
-            (need-fix (or (member program-name
-                                  `( ,find-program ,grep-program
-                                     "busybox" "curl" "ffmpeg" "git"
-                                     "make" "mpv"))
-                          (string= "compilation" command)
-                          (string= "*Find*" command)
-                          (string= "grep" command))))
-      (let ((process-coding-system-alist
-             `(("cmdproxy" utf-8 . ,locale-coding-system))))
-        (apply orig-fun args))
+  "Fix coding system for by change I/O coding-system."
+  (let* ((coding-system (my/find-shell-command-coding-system (car args)))
+         (coding-system-for-read (car coding-system))
+         (coding-system-for-write (cdr coding-system)))
     (apply orig-fun args)))
+(advice-add 'shell-command :around #'my/advice-shell-command-coding-fix)
 
 (defun my/output-coding-system-fix (input output)
-  "Fix coding system by `process-coding-system-alist'."
-  (if-let* ((program (car (split-string-shell-command input)))
-            (coding-system (alist-get program process-coding-system-alist
-                                      nil nil 'equal)))
-      (if (listp coding-system)
-          (decode-coding-string
-           (encode-coding-string
-            (decode-coding-string output (car coding-system))
-            (cdr coding-system))
-           'prefer-utf-8)
-        (decode-coding-string output 'prefer-utf-8))
+  "Fix coding system by convert string."
+  (if-let ((coding-system (my/find-shell-command-coding-system input)))
+      (decode-coding-string
+       (encode-coding-string
+        (decode-coding-string output (car coding-system))
+        (cdr coding-system))
+       'prefer-utf-8)
     output))
 
 (defun my/eshell-coding-system-fix (output)
   "Fix stdout coding-system in eshell."
-  (my/output-coding-system-fix (eshell-previous-input-string 0)
-                               output))
-;; (add-hook 'eshell-preoutput-filter-functions #'my/eshell-coding-system-fix nil)
-;; (remove-hook 'eshell-preoutput-filter-functions #'my/eshell-coding-system-fix nil)
+  (if-let ((command (condition-case nil
+                        (eshell-previous-input-string 0)
+                      (error nil))))
+      (my/output-coding-system-fix command output)
+    output))
+(add-hook 'eshell-preoutput-filter-functions #'my/eshell-coding-system-fix nil)
 
 (defun my/shell-coding-system-fix (output)
   "Fix stdout coding-system in shell."
   (my/output-coding-system-fix (comint-previous-input-string 0) output))
+(add-hook 'shell-mode-hook
+          (lambda ()
+            (add-hook 'comint-preoutput-filter-functions
+                      #'my/shell-coding-system-fix nil t)))
 
-(defun my/compilation-coding-system-fix (output)
+(defun my/compilation-coding-system-fix ()
   "Fix stdout coding-system in compilation."
-  (let ((fixed (my/output-coding-system-fix
-                (car compilation-arguments)
-                (buffer-substring compilation-filter-start (point)))))
-    (delete-region compilation-filter-start (point))
+  (let ((coding-system (my/find-shell-command-coding-system
+                        (car compilation-arguments))))
+    (setq-local coding-system-for-read (car coding-system)
+                coding-system-for-write (cdr coding-system))))
+(setq compilation-process-setup-function #'my/compilation-coding-system-fix)
+
+(defun my/grep-coding-system-fix ()
+  "Fix coding-system for grep."
+  (let ((coding-system (my/find-shell-command-coding-system ,grep-program)))
+    (setq-local coding-system-for-read (car coding-system))))
+(add-hook 'grep-setup-hook #'my/grep-coding-system-fix)
+
+(defun my/find-dired-coding-system-fix ()
+  "Fix coding-system for find-dired."
+  (let ((fixed (my/output-coding-system-fix find-program (buffer-string))))
+    (delete-region (point-min) (point-max))
     (insert fixed)))
-;; (with-eval-after-load 'compile
-;;   (add-hook 'compilation-filter-hook #'my/compilation-coding-system-fix))
+(let ((ori find-dired-refine-function))
+  (setq find-dired-refine-function 'my/find-dired-coding-system-fix)
+  (advice-add find-dired-refine-function :after ori))
 
 (defun my/advice-dired-shell-stuff-it (args)
   "Fix `;' cannot sequentially execute command on windows."
@@ -100,18 +111,16 @@
     (setq my/vanilla-shell shell-file-name
           shell-file-name "C:\\Windows\\system32\\bash.exe")))
 
-(dolist (fn '(shell-command start-file-process-shell-command))
-  (advice-add fn :around #'my/advice-shell-command-coding-fix))
-
-(add-hook 'shell-mode-hook
-          (lambda ()
-            (add-hook 'comint-preoutput-filter-functions
-                      #'my/shell-coding-system-fix nil t)))
-
 (setq default-process-coding-system '(utf-8-dos . utf-8-unix) ;; change this maybe break tramp sshx
       process-coding-system-alist
       `(("cmdproxy" . ,locale-coding-system)
+        ("curl" utf-8 . ,locale-coding-system)
+        ("ffmpeg" utf-8 . ,locale-coding-system)
+        (,find-program utf-8 . ,locale-coding-system)
+        (,grep-program utf-8 . ,locale-coding-system)
         ("git" utf-8 . ,locale-coding-system)
+        ("make" utf-8 . ,locale-coding-system)
+        ("mpv" utf-8 . ,locale-coding-system)
         ("sha256sum" utf-8 . ,locale-coding-system))
       file-name-coding-system locale-coding-system
       shr-use-fonts nil)
