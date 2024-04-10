010035f50237cd3864fbb70fba7a4568f4410f35
kkky <kkkykin@foxmail.com>
2024-04-10 13:55:31 +0800

    shell: env git_pager
    * .gitignore: sync eshell rc script
    
    * eshell/login: git_pager env in eshell
    
    * init_bash.sh (PROMPT_COMMAND): git_pager env on linux
    
    * init_cmdproxy.exe.sh: git_pager env on windows

--- init_bash.sh
+++ init_bash.sh
@@ -2,3 +2,5 @@ myprompt () {
     printf "\e]7;file://%s%s\e\\" "$HOSTNAME" "$PWD"
 }
 PROMPT_COMMAND=myprompt
+
+export GIT_PAGER=''
--- init_cmdproxy.exe.sh
+++ init_cmdproxy.exe.sh
@@ -1,5 +1,7 @@
 @echo off
 
+set GIT_PAGER=NUL
+
 rem Local Variables:
 rem mode: bat
 rem End:
