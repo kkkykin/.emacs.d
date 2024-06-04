5a0e2a82fcfe3eb5a4a36515792588d6673221c5
kkky <kkkykin@foxmail.com>
2024-06-04 09:38:01 +0800

    feat(shadow): project kill buffer exclude shadow file

--- init.el
+++ init.el
@@ -1330,7 +1330,26 @@
 (use-package shadowfile :defer 11
   :unless my/sys-android-p
   :config
-  (shadow-initialize))
+  (shadow-initialize)
+  (with-eval-after-load 'project
+    (cl-delete-if
+     (lambda (a) (member a '(buffer-file-name
+                        (and
+                         (major-mode . fundamental-mode)
+                         "\\`[^ ]"))))
+     project-kill-buffer-conditions)
+    (let ((shadow (rx bos (| (eval (file-name-nondirectory shadow-todo-file))
+                             (eval (file-name-nondirectory shadow-info-file)))
+                      eos)))
+      (dolist (k `((and buffer-file-name
+                        (not (and
+                              (major-mode . fundamental-mode)
+                              ,shadow)))
+                   (and
+                    (major-mode . fundamental-mode)
+                    "\\`[^ ]"
+                    (not ,shadow))))
+        (add-to-list 'project-kill-buffer-conditions k)))))
 
 (use-package treesit :defer 1
   :after prog-mode
