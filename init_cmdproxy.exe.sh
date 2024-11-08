fa0b3919428a228f8bd9525c88cb9395168139f4
kkky <kkkykin@foxmail.com>
2024-11-08 19:16:48 +0800

    chore(windows): cmd setup

--- init_cmdproxy.exe.sh
+++ init_cmdproxy.exe.sh
@@ -1,9 +1,6 @@
 @echo off
 
-set GIT_PAGER=NUL & set EDITOR=emacsclient
-set emacs_bin_dir="%emacs_dir%\usr\bin"
-md %emacs_bin_dir% & echo @cd /d %* > %emacs_bin_dir%\cdd.bat
-set emacs_bin_dir=
+set PAGER=NUL & set EDITOR=emacsclient
 
 rem Local Variables:
 rem mode: bat
