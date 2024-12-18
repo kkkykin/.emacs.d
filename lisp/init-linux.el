e70748ce37d27107da35cca082c488506b6ce326
kkky <kkkykin@foxmail.com>
2024-12-18 14:15:41 +0800

    fix(linux): plantuml args on nixos

--- lisp/init-linux.el
+++ lisp/init-linux.el
@@ -24,12 +24,33 @@
 
 ;;; Code:
 
-(defun my/transparency (value)
+(defun ml/transparency (value)
   "Sets the transparency of the frame window. 0=transparent/100=opaque"
   (interactive "nTransparency Value 0 - 100 opaque:")
   (set-frame-parameter nil 'alpha-background value))
 
-(add-hook 'server-after-make-frame-hook #'my/setup-faces)
+(add-hook 'server-after-make-frame-hook #'ml/setup-faces)
+
+(defun ml/fix-plantuml-args (path args)
+  "Fix plantuml Warning: -headless flag must be the first one in the
+command line."
+  (when-let* ((link (file-symlink-p (executable-find "plantuml"))))
+    (with-temp-buffer
+      (insert-file-contents link)
+      (re-search-forward "^exec .+")
+      (let ((cmd (split-string-shell-command (match-string 0))))
+        (set path (cadr cmd))
+        (set args (append (symbol-value args) (cl-subseq cmd 2 -2)))))))
+
+(with-eval-after-load 'ob-plantuml
+  (ml/fix-plantuml-args 'org-plantuml-executable-path 'org-plantuml-args))
+
+(with-eval-after-load 'plantuml-mode
+  (ml/fix-plantuml-args 'plantuml-executable-path 'plantuml-executable-args))
 
 (provide 'init-linux)
 ;;; init-linux.el ends here
+
+;; Local Variables:
+;; read-symbol-shorthands: (("ml/" . "my/linux-"))
+;; End:
