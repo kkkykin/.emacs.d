ea6d4c2024df97ec438907ac0f68f43fd930b386
kkky <kkkykin@foxmail.com>
2025-01-15 00:13:01 +0800

    feat(rss): auto compile

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,8 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((prog-mode . ((tags-table-list . ("~/.emacs.d/TAGS")))))
+((prog-mode . ((tags-table-list . ("~/.emacs.d/TAGS"))))
+ ("lisp/init-rss.el.gpg"
+  . ((emacs-lisp-mode
+      . ((eval . (add-hook 'after-save-hook
+                           #'elisp-byte-compile-file nil t)))))))
